9bee1a60540f23574d5b0f21801d67585f0a73c1
Stepan Dordiai <stepandordiai@gmail.com>
2026-04-24 00:41:44 +0200

    feat: update thermostat page

--- frontend/src/app/[locale]/thermostat/page.tsx
+++ frontend/src/app/[locale]/thermostat/page.tsx
@@ -41,23 +41,74 @@ export default async function Thermostat() {
 				subheading={t("thermostat.subtitle")}
 				imgSrc="https://kenticoprod.azureedge.net/kenticoblob/crestron/media/crestron/generalsiteimages/residential_enduser_new/product-thermostat-thumn.jpg"
 			/>
-			<section className="section" id="section">
-				<h2 className="section__title">Peace of mind.</h2>
-				<div
-					style={{
-						display: "grid",
-						gridTemplateColumns: "repeat(2, 1fr)",
-						gap: 40,
-					}}
-				>
+			<section className="technology-section" id="section">
+				<h2 className="section__title">The perfect temperature, always</h2>
+				<div className="technology-section-container">
 					<p>
-						The greatest stress reliever is knowing your home is safe and secure
-						for you and your loved ones. Crestron Home unifies the technologies
-						that make it a reality, from security systems to power and energy
-						management that prevents service disruption. No matter what happens
-						– storms, heat waves, equipment failures – your home will work
-						exactly as it was designed to.
+						Our bodies have a basic need to maintain a temperature within a
+						specific range; if it’s too hot or cold in the home, especially at
+						bedtime, this can be challenging. Horizon® smart thermostats, paired
+						with multi-zone heating and cooling systems, maintain an optimal
+						temperature at all times, improving your comfort and quality of
+						sleep.
 					</p>
+					<div style={{ position: "relative" }}>
+						<img
+							style={{
+								position: "absolute",
+								height: "100%",
+								bottom: "-25px",
+								left: "-10px",
+							}}
+							src="/05-c.png"
+							alt=""
+						/>
+						<img
+							style={{
+								borderRadius: 10,
+							}}
+							src="/05.jpg"
+							alt=""
+						/>
+					</div>
+				</div>
+			</section>
+			<section className="technology-section" id="section">
+				<h2 className="section__title">Thoughtfully designed</h2>
+				<div className="technology-section-container">
+					<img
+						style={{
+							borderRadius: 10,
+						}}
+						src="https://kenticoprod.azureedge.net/kenticoblob/crestron/media/crestron/generalsiteimages/residential_enduser_new/product-thermostats-thoughtfully-designed.jpg"
+						alt=""
+					/>
+					<p>
+						Horizon Thermostats feature Wi-Fi® communication, making them
+						equally simple to install for new construction or retrofit. There
+						are no batteries to change, so you can set it and forget it.
+					</p>
+				</div>
+			</section>
+			<section className="technology-section" id="section">
+				<h2 className="section__title">Schedule routines</h2>
+				<div className="technology-section-container">
+					<p>
+						Ensure just the right temperature when you wake, to welcome you home
+						from work, or for sleep at night
+					</p>
+					<img
+						style={{
+							borderRadius: 10,
+						}}
+						src="https://kenticoprod.azureedge.net/kenticoblob/crestron/media/crestron/generalsiteimages/residential_enduser_new/product-thermostats-schedule-routine.jpg"
+						alt=""
+					/>
+				</div>
+			</section>
+			<section className="technology-section" id="section">
+				<h2 className="section__title">Controlled Remotely</h2>
+				<div className="technology-section-container">
 					<img
 						style={{
 							borderRadius: 10,
@@ -65,6 +116,12 @@ export default async function Thermostat() {
 						src="/05.jpg"
 						alt=""
 					/>
+					<p>
+						Using remote temperature and humidity sensors, thermostats can be
+						hidden in a central closet and controlled remotely from mobile
+						devices, multi-function keypads, remotes, and touch screens. Sleek,
+						modern Horizon Thermostats complement almost any décor.
+					</p>
 				</div>
 			</section>
 			<TechnologyProducts technology="Thermostat" />
