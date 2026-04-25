4601a7e09c7a77c1274b422b13ac07ef2d1a75f8
Stepan Dordiai <stepandordiai@gmail.com>
2026-04-25 17:01:07 +0200

    feat: update products.ts api

--- frontend/src/lib/api/products.ts
+++ frontend/src/lib/api/products.ts
@@ -1,5 +1,16 @@
+interface Product {
+	id: number;
+	img: string;
+	name: string;
+	type: string;
+	technology: string;
+	brand: string;
+	is_active: boolean;
+}
+
 export const getProducts = async () => {
 	const res = await fetch("https://pavision-backend.onrender.com/products");
 	if (!res.ok) throw new Error("Failed to fetch products");
-	return res.json();
+	const data = await res.json();
+	return data.filter((product: Product) => product.is_active);
 };
