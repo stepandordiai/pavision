f64b6f451642f680878851daa52364a872d1c262
Stepan Dordiai <stepandordiai@gmail.com>
2026-05-25 01:28:13 +0200

    feat: update form in contacts page

--- frontend/src/app/[locale]/contacts/ContactsClient.tsx
+++ frontend/src/app/[locale]/contacts/ContactsClient.tsx
@@ -1,75 +1,135 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+import { supabase } from "@/lib/supabase";
 import { useState } from "react";
 
+interface Client {
+	name: string;
+	tel: string;
+	email: string;
+	message: string;
+}
+
+const INIT_FORM = {
+	name: "",
+	tel: "",
+	email: "",
+	message: "",
+};
+
 export default function ContactsClient() {
-	const [form, setForm] = useState({
-		name: "",
-		tel: "",
-		email: "",
-		message: "",
-	});
+	const t = useTranslations("contacts");
+
+	const [formError, setFormError] = useState<string | null>(null);
+	const [loading, setLoading] = useState(false);
+	const [success, setSuccess] = useState(false);
+	const [form, setForm] = useState(INIT_FORM);
 
 	const handleForm = (name: string, value: string) => {
 		setForm((prev) => ({ ...prev, [name]: value }));
 	};
 
+	// TODO: LEARN THIS
+	// Supabase
+	const insertClient = async (data: Client) => {
+		setFormError(null);
+		setLoading(true);
+
+		const { error } = await supabase.from("clients").insert([data]);
+		if (error) {
+			if (error.code !== "23505") {
+				setFormError(error.message);
+				console.error("Insert error:", error.message);
+				setLoading(false);
+				return;
+			}
+		}
+		setLoading(false);
+		setSuccess(true);
+
+		setTimeout(() => {
+			setSuccess(false);
+			setForm(INIT_FORM);
+		}, 3000);
+	};
+
 	return (
-		<form action="mailto:info@pavision.cz" className="form">
-			<h2 style={{ fontSize: "22px", fontWeight: 500 }}>Kontakt formular</h2>
+		<form
+			// TODO: learn this
+			onSubmit={async (e) => {
+				e.preventDefault();
+				await insertClient(form);
+			}}
+			className="form"
+		>
+			<h2 style={{ fontSize: "22px", fontWeight: 500 }}>{t("title")}</h2>
+			{formError && <span style={{ color: "red" }}>{formError}</span>}
 			<div
 				className={`input-container ${form.name !== "" ? "input-container--active" : ""}`}
 			>
-				<label htmlFor="name">Jmeno a prijmeni</label>
+				<label htmlFor="name">
+					{t("name")} ({t("required")})
+				</label>
 				<input
 					onChange={(e) => handleForm(e.target.name, e.target.value)}
+					value={form.name}
 					name="name"
-					className={`input ${form.name !== "" ? "input--active" : ""}`}
 					type="text"
 					id="name"
 					autoComplete="name"
+					disabled={success}
+					required
 				/>
 			</div>
 			<div
 				className={`input-container ${form.tel !== "" ? "input-container--active" : ""}`}
 			>
-				<label htmlFor="tel">Telefon (required)</label>
+				<label htmlFor="tel">
+					{t("tel")} ({t("required")})
+				</label>
 				<input
 					onChange={(e) => handleForm(e.target.name, e.target.value)}
+					value={form.tel}
 					name="tel"
-					className="input"
 					type="text"
 					id="tel"
-					autoComplete="name"
+					autoComplete="tel"
+					disabled={success}
+					required
 				/>
 			</div>
 			<div
 				className={`input-container ${form.email !== "" ? "input-container--active" : ""}`}
 			>
-				<label htmlFor="email">Email</label>
+				<label htmlFor="email">Email ({t("required")})</label>
 				<input
 					onChange={(e) => handleForm(e.target.name, e.target.value)}
+					value={form.email}
 					name="email"
-					className="input"
-					type="text"
+					type="email"
 					id="email"
-					autoComplete="name"
+					autoComplete="email"
+					disabled={success}
+					required
 				/>
 			</div>
 			<div
 				style={{ height: "100%" }}
 				className={`input-container ${form.message !== "" ? "input-container--active" : ""}`}
 			>
-				<label htmlFor="message">Dopis</label>
+				<label htmlFor="message">{t("message")}</label>
 				<textarea
 					onChange={(e) => handleForm(e.target.name, e.target.value)}
+					value={form.message}
 					name="message"
-					className="textarea input"
+					className="textarea"
 					id="message"
+					disabled={success}
 				></textarea>
 			</div>
-			<button className="form__btn" type="submit">
-				Poslat
+			<button className="form__btn" type="submit" disabled={success}>
+				{loading ? t("loading") : success ? t("success") : t("submit")}
 			</button>
 		</form>
 	);
