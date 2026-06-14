dd54f4a329c034a03aa17a44e02cc08df0a1df26
Stepan Dordiai <stepandordiai@gmail.com>
2026-06-15 00:22:21 +0200

    feat: add Brands component

--- frontend/src/app/[locale]/HomeClient.tsx
+++ frontend/src/app/[locale]/HomeClient.tsx
@@ -3,24 +3,8 @@ import Technologies from "@/components/home/Technologies/Technologies";
 import OurSolutions from "@/components/home/OurSolutions/OurSolutions";
 import WhatWeDo from "@/components/WhatWeDo/WhatWeDo";
 import Testimonials from "@/components/Testimonials/Testimonials";
+import Brands from "@/components/Brands/Brands";
 import "./Home.scss";
-
-const brands = [
-	"Crestron",
-	"Loxone",
-	"Lutron",
-	"Denon",
-	"Marantz",
-	"LG",
-	"Sonos",
-	"Bowers & Wilkins",
-	"Ubiquiti",
-	"MicroTik",
-	"Cisco",
-	"Jablotron",
-	"Paradox",
-	"Risco",
-];
 
 export default function HomeClient() {
 	return (
@@ -30,14 +14,7 @@ export default function HomeClient() {
 			<WhatWeDo />
 			<Testimonials />
 			<Technologies />
-			<section className="section">
-				<h2 className="section__title">Trusted Brands. Smarter Homes.</h2>
-				<div className="brands">
-					{brands.map((brand, i) => {
-						return <div key={i}>{brand}</div>;
-					})}
-				</div>
-			</section>
+			<Brands />
 		</main>
 	);
 }
