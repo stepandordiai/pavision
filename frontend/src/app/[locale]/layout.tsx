0a87aae2e0a73a9c9538297b8fe9c690f328dcb5
Stepan Dordiai <stepandordiai@gmail.com>
2026-08-09 17:31:55 +0200

    feat: add JSON-LD to layout

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import { Poppins, Michroma } from "next/font/google";
-import { Metadata } from "next";
+import { type Metadata } from "next";
 import { hasLocale, NextIntlClientProvider } from "next-intl";
 import Header from "@/components/layout/Header/Header";
 import Footer from "@/components/layout/Footer/Footer";
@@ -34,6 +34,82 @@ export const metadata: Metadata = {
 	},
 };
 
+// TODO: learn this
+export function generateJsonLd(locale: string) {
+	return {
+		"@context": "https://schema.org",
+		"@graph": [
+			{
+				"@type": [
+					"LocalBusiness",
+					"ElectronicsStore",
+					"HomeAndConstructionBusiness",
+				],
+				"@id": `${BASE_URL}/#business`,
+				name: "P&A Vision s.r.o.",
+				url: BASE_URL,
+				logo: `${BASE_URL}/logo.svg`,
+				image: `${BASE_URL}/pavision-og.png`,
+				telephone: ["+420775632426", "+420777049617"],
+				email: "info@pavision.cz",
+				address: {
+					"@type": "PostalAddress",
+					streetAddress: "Soběslavova 1381",
+					addressLocality: "Kladno - Kročehlavy",
+					postalCode: "272 01",
+					addressCountry: "CZ",
+				},
+				vatID: "CZ23654341",
+				taxID: "23654341",
+				sameAs: [
+					"https://www.instagram.com/pa_vision.cz",
+					"https://www.facebook.com/profile.php?id=61581254326915",
+				],
+				areaServed: ["CZ", "EU"],
+				priceRange: "$$$",
+				// TODO:
+				makesOffer: [
+					{
+						"@type": "Offer",
+						itemOffered: { "@type": "Service", name: "Crestron integration" },
+					},
+					{
+						"@type": "Offer",
+						itemOffered: { "@type": "Service", name: "Loxone smart home" },
+					},
+					{
+						"@type": "Offer",
+						itemOffered: { "@type": "Service", name: "Home automation" },
+					},
+					{
+						"@type": "Offer",
+						itemOffered: {
+							"@type": "Service",
+							name: "Audio/Video installation",
+						},
+					},
+					{
+						"@type": "Offer",
+						itemOffered: { "@type": "Service", name: "Network infrastructure" },
+					},
+					{
+						"@type": "Offer",
+						itemOffered: { "@type": "Service", name: "Security systems" },
+					},
+				],
+			},
+			{
+				"@type": "WebSite",
+				"@id": `${BASE_URL}/#website`,
+				url: BASE_URL,
+				name: "P&A Vision",
+				publisher: { "@id": `${BASE_URL}/#business` },
+				inLanguage: locale,
+			},
+		],
+	};
+}
+
 export default async function LocaleLayout({
 	children,
 	params,
@@ -46,8 +122,16 @@ export default async function LocaleLayout({
 		return notFound();
 	}
 
+	const jsonLd = generateJsonLd(locale);
+
 	return (
 		<html lang={locale}>
+			<head>
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+				/>
+			</head>
 			<body className={`${poppins.variable} ${michroma.variable}`}>
 				<ScrollToTop />
 				<ScrollToTopBtn />
